633b95f39c64d976829e05230f0d95ce2b23aa57
modderlucky <kori104suneel@gmail.com>
2024-05-18 08:39:08 +0530

    Update Ns_Followers.py

--- Ns_Followers.py
+++ Ns_Followers.py
@@ -11,9 +11,9 @@ class Ns_Followers:
         self.requests=requests
         self.__AES_NoPadding = AES_CBC_NoPadding()
 
-        self.__userid='...' #Enter UserID Here
-        self.__username='...' #Enter UserName Here
-        self._session='...' #Enter SessionID Here
+        self.__userid='66993976111' #Enter UserID Here
+        self.__username='mrhacker758401' #Enter UserName Here
+        self._session='66993976111:9cqxw9qPIlxdvO:29:AYfKeknSGmlEpOmoEMo7ehY7q_3DKZ1GhaDf9nBD6w' #Enter SessionID Here
 
         self.__Pk=self.__userid.encode()+b'*'+str(random.randint(111,999)).encode()
         self.__Ip=os.urandom(8).hex().encode()+b'*'+str(random.randint(111,999)).encode()
